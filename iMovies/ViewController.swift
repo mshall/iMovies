a750be66ce7ba0a990d6e63ef0ff9567275c14b2
Mohamed S. El-Shall <mohamed.s.elshall2011@gmail.com>
2018-03-20 19:04:05 +0200

    Initial commit and starting the project

---
+++ iMovies/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iMovies
+//
+//  Created by Mohamed S. El-Shall on 3/20/18.
+//  Copyright © 2018 Mohamed S. El-Shall. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
